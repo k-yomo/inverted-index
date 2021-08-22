bdd75f91ec69ee4c5b82ce72e56a0f4f7de2db02
k-yomo <k-yomoda@mercari.com>
2021-08-22 13:07:47 +0900

    temp

--- index/index.go
+++ index/index.go
@@ -5,20 +5,59 @@ const (
 	NegativeInf = -Inf - 1           // use min int as -∞
 )
 
-type Index struct {
-	postingMap   map[string][]int
-	postingCache map[string]int
+type Document struct {
+	ID   int
+	Text string
 }
 
-func NewIndex(document string) *Index {
-	index := make(map[string][]int)
-	for i, term := range analyze(document) {
-		index[term] = append(index[term], i)
+type Index struct {
+	tokenPostingInfoMap map[string]*postingInfo
+	docNum              int
+}
+
+type postingInfo struct {
+	postings     postings
+	docFrequency float64
+}
+
+func NewIndex(documents ...Document) *Index {
+	tokenPostingInfoMap := make(map[string]*postingInfo)
+	for _, doc := range documents {
+		postingMap := make(map[string][]int)
+		terms := tokenize(doc.Text)
+		for i, term := range terms {
+			postingMap[term] = append(postingMap[term], i)
+		}
+		for term, postings := range postingMap {
+			if tokenPostingInfoMap[term] == nil {
+				tokenPostingInfoMap[term] = &postingInfo{}
+			}
+			tokenPostingInfoMap[term].postings = append(tokenPostingInfoMap[term].postings, &posting{
+				docID:         doc.ID,
+				termFrequency: float64(len(postings)) / float64(len(terms)),
+				postings:      postings,
+			})
+		}
 	}
+
+	for _, postingInfo := range tokenPostingInfoMap {
+		postingInfo.docFrequency = float64(len(postingInfo.postings)) / float64(len(documents))
+	}
+
 	return &Index{
-		postingMap:   index,
-		postingCache: make(map[string]int),
+		tokenPostingInfoMap: tokenPostingInfoMap,
+		docNum:              len(documents),
 	}
+}
+
+type PhrasePosition struct {
+	docID         int
+	positionRange *Range
+}
+
+type TermPosition struct {
+	docID    int
+	position int
 }
 
 type Range struct {
@@ -26,110 +65,60 @@ type Range struct {
 	To   int
 }
 
-func (idx *Index) NextPhrase(phrase string, position int) *Range {
-	terms := analyze(phrase)
+type Position struct {
+	docID    int
+	position int
+}
+
+func (idx *Index) NextPhrase(phrase string, position *Position) *PhrasePosition {
+	terms := tokenize(phrase)
 	termNum := len(terms)
 	if termNum == 0 {
-		return &Range{From: Inf, To: Inf}
+		return &PhrasePosition{
+			docID:         Inf,
+			positionRange: &Range{From: Inf, To: Inf},
+		}
 	}
 
 	from := idx.Next(terms[0], position)
+	if from.docID == Inf {
+		return &PhrasePosition{
+			docID:         Inf,
+			positionRange: &Range{From: Inf, To: Inf},
+		}
+	}
 	to := from
 	for i := 1; i < len(terms); i++ {
 		to = idx.Next(terms[i], to)
 	}
-	if to == Inf {
-		return &Range{From: Inf, To: Inf}
-	}
 
-	if to-from == termNum-1 {
-		return &Range{From: from, To: to}
+	if to.docID == from.docID && to.position-from.position == termNum-1 {
+		return &PhrasePosition{
+			docID:         from.docID,
+			positionRange: &Range{From: from.position, To: to.position},
+		}
 	}
 	return idx.NextPhrase(phrase, from)
 }
 
-func (idx *Index) First(term string) int {
-	postings := idx.postingMap[term]
-	if len(postings) == 0 {
-		return NegativeInf
-	}
-	return postings[0]
-}
-
-func (idx *Index) Last(term string) int {
-	postings := idx.postingMap[term]
-	if len(postings) == 0 {
-		return Inf
-	}
-	return postings[len(postings)-1]
-}
-
-func (idx *Index) Prev(term string, current int) int {
-	postings, exist := idx.postingMap[term]
-	if !exist || current <= postings[0] {
-		return NegativeInf
-	}
-	if current > postings[len(postings)-1] {
-		return postings[len(postings)-1]
+func (idx *Index) Next(term string, position *Position) *Position {
+	if position.docID == Inf {
+		return &Position{docID: Inf, position: Inf}
 	}
 
-	ok, ng := 0, len(postings)-1
-	for ng-ok > 1 {
-		mid := (ok + ng) / 2
-		if postings[mid] < current {
-			ok = mid
-		} else {
-			ng = mid
+	pi := idx.tokenPostingInfoMap[term]
+	for position.docID != Inf {
+
+	}
+	if position.docID < pi.postings[0].docID {
+		pos := pi.postings[0].Next(position.position)
+		if pos == Inf {
+
 		}
 	}
-
-	return postings[ok]
-}
-
-func (idx *Index) Next(term string, current int) int {
-	last := idx.Last(term)
-	postings, exist := idx.postingMap[term]
-	if !exist || current >= last {
-		return Inf
+	postingInfo, ok := idx.tokenPostingInfoMap[term]
+	if !ok {
+		return &Position{docID: Inf, position: Inf}
 	}
-	if current < postings[0] {
-		idx.setPostingCache(term, 0)
-		return postings[0]
-	}
-
-	low := 0
-	if cache := idx.postingCache[term]; cache > 0 && postings[cache-1] <= current {
-		low = cache - 1
-	}
-
-	jump := 1
-	high := low + jump
-	for high < len(postings)-1 && postings[high] <= current {
-		low = high
-		jump = 2 * jump
-		high = low + jump
-	}
-	if high > last {
-		high = last
-	}
-
-	nextIndex := binarySearch(postings, low, high, current)
-	idx.setPostingCache(term, nextIndex)
-	return postings[nextIndex]
-}
-
-func (idx *Index) setPostingCache(term string, i int) {
-	idx.postingCache[term] = i
-}
-
-func binarySearch(postings []int, low, high, current int) int {
-	for high-low > 1 {
-		mid := (low + high) / 2
-		if postings[mid] > current {
-			high = mid
-		} else {
-			low = mid
-		}
-	}
-	return high
+	postingInfo.postings
 }
---
+++ index/posting.go
@@ -0,0 +1,103 @@
+package index
+
+type postings []*posting
+
+type posting struct {
+	docID         int
+	termFrequency float64
+	postings      []int
+	postingCache  int
+}
+
+func (ps postings) DocLeft(docID int) int {
+	if len(ps) == 0 || docID == ps[0].docID {
+		return NegativeInf
+	}
+}
+
+func (ps postings) DocRight(docID int) int {
+	if len(ps) == 0 || docID == ps[len(ps)-1].docID {
+		return Inf
+	}
+
+}
+
+func (p *posting) First() int {
+	if len(p.postings) == 0 {
+		return NegativeInf
+	}
+	return p.postings[0]
+}
+
+func (p *posting) Last() int {
+	if len(p.postings) == 0 {
+		return Inf
+	}
+	return p.postings[len(p.postings)-1]
+}
+
+func (p *posting) Prev(current int) int {
+	postings := p.postings
+	if current <= postings[0] {
+		return NegativeInf
+	}
+	if current > postings[len(postings)-1] {
+		return postings[len(postings)-1]
+	}
+
+	ok, ng := 0, len(postings)-1
+	for ng-ok > 1 {
+		mid := (ok + ng) / 2
+		if postings[mid] < current {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return postings[ok]
+}
+
+func (p *posting) Next(current int) int {
+	last := p.Last()
+	postings := p.postings
+	if current >= last {
+		return Inf
+	}
+	if current < postings[0] {
+		p.postingCache = 0
+		return postings[0]
+	}
+
+	low := 0
+	if cache := p.postingCache; cache > 0 && postings[cache-1] <= current {
+		low = cache - 1
+	}
+
+	jump := 1
+	high := low + jump
+	for high < len(postings)-1 && postings[high] <= current {
+		low = high
+		jump = 2 * jump
+		high = low + jump
+	}
+	if high > last {
+		high = last
+	}
+
+	nextIndex := binarySearch(postings, low, high, current)
+	p.postingCache = nextIndex
+	return postings[nextIndex]
+}
+
+func binarySearch(postings []int, low, high, current int) int {
+	for high-low > 1 {
+		mid := (low + high) / 2
+		if postings[mid] > current {
+			high = mid
+		} else {
+			low = mid
+		}
+	}
+	return high
+}
