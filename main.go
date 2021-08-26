9c1b487ca7024f8034c226898a57cd3b8db0e68a
k-yomo <kanji.yy@gmail.com>
2021-08-26 18:56:41 +0900

    Implement TF-IDF ranking

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"github.com/k-yomo/inverted-index/analyzer"
 	"github.com/k-yomo/inverted-index/index"
+	"github.com/k0kubun/pp"
 )
 
 func main()  {
 	docs := []index.Document{
-		{ ID: 1, Text: "there is a black cat"},
+		{ ID: 1, Text: "there is a white cat"},
 		{ ID: 2, Text: "black hair cat"},
 		{ ID: 3, Text: "black cat"},
+		{ ID: 3, Text: "white dog"},
 	}
+
 	idx := index.NewIndex(&analyzer.EnglishAnalyzer{}, docs...)
-	docIDs := idx.PhraseSearch("hair cat")
-	fmt.Println(docIDs)
+	hits := idx.Search("black cat")
+
+	pp.Println(hits)
 }
